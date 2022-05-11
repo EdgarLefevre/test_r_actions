cd581189dad93872edb5a758e9bcadb979d923c7
Edgar <lefevreedg@gmail.com>
2022-05-11 13:59:57 +0200

    does it works ?

--- R/main.r
+++ R/main.r
@@ -1,9 +1,17 @@
 #!/usr/bin/env Rscript
-# # Automated to run from terminal, see:
-#   * README_scr2.md *
-# Initial verification, creates output directories,
-# checks biotypes,
 # OUTPUT:  saves dds object into rds/ folder
 # note : obj@row_data may have symbol_unique but empty external_gene_name:
 #                      because when symbol retrieved by ncbi , not biomart.
 # johaGL 2022
+
+#' Add together two numbers
+#'
+#' @param x A number
+#' @param y A number
+#' @return The sum of \code{x} and \code{y}
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+add <- function(x, y) {
+  x + y
+}
